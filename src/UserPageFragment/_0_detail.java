b26199537f84aac2d01cd65d745d5999fae02155
ロリ幼女†杏鈴愛で隊† <tao@192.168.10.5>
2015-04-05 02:10:29 +0900

    Lightning of Dark
    ユーザーページのbioに＠付きのアカウント名があった場合、リンクできるようにした

--- src/UserPageFragment/_0_detail.java
+++ src/UserPageFragment/_0_detail.java
@@ -1,10 +1,16 @@
 package UserPageFragment;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.tao.lightning_of_dark.R;
 import com.tao.lightning_of_dark.UserPage;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,7 +29,26 @@ public class _0_detail extends Fragment {
 		return v;
 	}
 	public void setText(){
-		UserBio.setText(UserPage.target.getDescription());
+		String bio = UserPage.target.getDescription();
+		Matcher at_user = Pattern.compile("@\\w*", Pattern.DOTALL).matcher(bio);
+		Matcher url = Pattern.compile("(http://|https://){1}[\\w\\.\\-/:\\#\\?\\=\\&\\;\\%\\~\\+]+", Pattern.DOTALL).matcher(bio);
+		if(at_user.find() || url.find()){
+			at_user.reset();
+			url.reset();
+			if(at_user.find()){
+				at_user.reset();
+				while(at_user.find())
+					bio = bio.replace(at_user.group(), "<a href=\"https://twitter.com/" + at_user.group().substring(1) + "\">" + at_user.group() + "</a>");
+			}
+			if(url.find()){
+				url.reset();
+				while(url.find())
+					bio = bio.replace(url.group(), "<a href=\"" + url.group() + "\">" + url.group() + "</a>");
+			}
+			UserBio.setMovementMethod(LinkMovementMethod.getInstance());
+			UserBio.setText(Html.fromHtml(bio));
+		}else
+			UserBio.setText(bio);
 		location.setText(UserPage.target.getLocation());
 		Link.setText(UserPage.target.getURL());
 	}
