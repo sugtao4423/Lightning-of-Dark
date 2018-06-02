c92751b360741b9629b0e5339a2705ea5bf965f5
sugtao4423 <sugtao4423@gmail.com>
2018-06-03 01:06:06 +0900

    bug fix
    TL上で見た会話に横から失礼リプするときにタップした人しかmentionにならない問題をfix

--- src/sugtao4423/lod/dialog_onclick/Dialog_reply.java
+++ src/sugtao4423/lod/dialog_onclick/Dialog_reply.java
@@ -1,18 +1,24 @@
 package sugtao4423.lod.dialog_onclick;
 
 import twitter4j.Status;
+import twitter4j.UserMentionEntity;
+
+import java.util.ArrayList;
+
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import sugtao4423.lod.ApplicationClass;
 import sugtao4423.lod.TweetActivity;
 
 public class Dialog_reply implements OnClickListener{
 
 	private Status status;
 	private Context context;
+	private String myScreenName;
 
 	private AlertDialog dialog;
 
@@ -20,6 +26,7 @@ public class Dialog_reply implements OnClickListener{
 		this.status = status;
 		this.context = context;
 		this.dialog = dialog;
+		this.myScreenName = ((ApplicationClass)context.getApplicationContext()).getMyScreenName();
 	}
 
 	@Override
@@ -28,7 +35,14 @@ public class Dialog_reply implements OnClickListener{
 
 		Status item = status.isRetweet() ? status.getRetweetedStatus() : status;
 
-		if(item.getUserMentionEntities().length > 1)
+		ArrayList<String> mentions = new ArrayList<String>();
+		mentions.add(item.getUser().getScreenName());
+		for(UserMentionEntity entity : item.getUserMentionEntities()){
+			if(!entity.getScreenName().equals(myScreenName) && mentions.indexOf(entity.getScreenName()) == -1)
+				mentions.add(entity.getScreenName());
+		}
+
+		if(mentions.size() > 1)
 			selectReplyDialog(item);
 		else
 			reply(item);
