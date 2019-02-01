7a0f175ad8fb1316076a58a3ec886ca48942192b
sugtao4423 <sugtao4423@gmail.com>
2019-02-02 02:48:12 +0900

    正規表現のスラッシュのエスケープを除去

--- app/src/main/java/sugtao4423/lod/utils/Regex.java
+++ app/src/main/java/sugtao4423/lod/utils/Regex.java
@@ -7,8 +7,8 @@ public class Regex{
     public static final Pattern media_image = Pattern.compile("http(s)?://pbs.twimg.com/media/");
     public static final Pattern media_video = Pattern.compile("http(s)?://video.twimg.com/ext_tw_video/[0-9]+/(pu|pr)/vid/.+/.+(.mp4|.webm)");
     public static final Pattern media_gif = Pattern.compile("http(s)?://pbs.twimg.com/tweet_video/");
-    public static final Pattern shareUrl = Pattern.compile("http(s)?:\\/\\/twitter.com\\/intent\\/tweet\\?.+");
     public static final Pattern statusUrl = Pattern.compile("http(s)?://twitter.com/(i/web|[0-9a-zA-Z_]+)/status/([0-9]+)");
+    public static final Pattern shareUrl = Pattern.compile("http(s)?://twitter.com/intent/tweet\\?.+");
     public static final Pattern userUrl = Pattern.compile("http(s)?://twitter.com/([0-9a-zA-Z_]+)");
     public static final Pattern userIconUrl = Pattern.compile("^http(s)?://pbs.twimg.com/.+/+(.+)(\\..+)$");
     public static final Pattern userBannerUrl = Pattern.compile("^http(s)?://pbs.twimg.com/profile_banners/[0-9]+/([0-9]+)/web_retina$");
