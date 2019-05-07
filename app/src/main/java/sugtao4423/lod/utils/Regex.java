cba7bd08a8ae4513d2fa4468eda1d67158cc030c
sugtao4423 <sugtao4423@gmail.com>
2019-05-07 14:29:07 +0900

    ユーザープロフィールのバナーを保存できない問題をfix

--- app/src/main/java/sugtao4423/lod/utils/Regex.java
+++ app/src/main/java/sugtao4423/lod/utils/Regex.java
@@ -15,7 +15,7 @@ public class Regex{
     public static final Pattern userIconUrl = Pattern.compile("^http(s)?://pbs.twimg.com/.+/+(.+)(\\..+)$");
     public static final int userIconUrlFileNameGroup = 2;
     public static final int userIconUrlDotExtGroup = 3;
-    public static final Pattern userBannerUrl = Pattern.compile("^http(s)?://pbs.twimg.com/profile_banners/[0-9]+/([0-9]+)/web_retina$");
+    public static final Pattern userBannerUrl = Pattern.compile("^http(s)?://pbs.twimg.com/profile_banners/[0-9]+/([0-9]+)/");
     public static final int userBannerUrlFileNameGroup = 2;
     public static final Pattern userAndAnyUrl = Pattern.compile("@[0-9a-zA-Z_]+|(http://|https://){1}[\\w\\.\\-/:\\#\\?\\=\\&\\;\\%\\~\\+]+", Pattern.DOTALL);
 
