a8173b8fda3d6f462413d6aa38e26ac0e1be90d3
sugtao4423 <sugtao4423@gmail.com>
2019-07-25 02:48:44 +0900

    ShowToastのリファクタリング
    UserStreamがなくなってふぁぼ通知などが来ないのでImageViewを使う必要がなくなり削除

--- app/src/main/java/sugtao4423/lod/ShowToast.kt
+++ app/src/main/java/sugtao4423/lod/ShowToast.kt
@@ -1,49 +1,26 @@
 package sugtao4423.lod
 
 import android.content.Context
-import android.graphics.Color
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
-import com.loopj.android.image.SmartImageView
 
-class ShowToast(context: Context, text: String, duration: Int, toastType: Type) : Toast(context) {
+class ShowToast(context: Context, text: String, duration: Int) : Toast(context) {
 
-    constructor(context: Context, resId: Int, duration: Int, toastType: Type) : this(context, context.getString(resId), duration, toastType)
+    constructor(context: Context, resId: Int, duration: Int) : this(context, context.getString(resId), duration)
 
-    constructor(context: Context, text: String, duration: Int) : this(context, text, duration, Type.NORMAL)
+    constructor(context: Context, text: String) : this(context, text, LENGTH_SHORT)
 
-    constructor(context: Context, resId: Int, duration: Int) : this(context, resId, duration, Type.NORMAL)
-
-    constructor(context: Context, text: String) : this(context, text, LENGTH_SHORT, Type.NORMAL)
-
-    constructor(context: Context, resId: Int) : this(context, resId, LENGTH_SHORT, Type.NORMAL)
-
-    enum class Type {
-        NORMAL
-
-    }
+    constructor(context: Context, resId: Int) : this(context, resId, LENGTH_SHORT)
 
     private val v: View = View.inflate(context, R.layout.custom_toast, null)
-    private val mainMessage: TextView = v.findViewById(R.id.toast_main_message)
-    private val tweet: TextView = v.findViewById(R.id.toast_tweet)
-    private val icon: SmartImageView = v.findViewById(R.id.toast_icon)
+    private val message: TextView = v.findViewById(R.id.toast_message)
 
     init {
         view = v
 
-        when (toastType) {
-            Type.NORMAL -> normalToast(text, duration)
-        }
-    }
-
-    private fun normalToast(text: String, duration: Int) {
-        mainMessage.text = text
-        mainMessage.setTextColor(Color.WHITE)
-        mainMessage.setPadding(3, 2, 3, 2)
-
-        tweet.visibility = View.GONE
-        icon.visibility = View.GONE
+        message.text = text
+        message.setPadding(3, 2, 3, 2)
 
         setDuration(duration)
         show()
