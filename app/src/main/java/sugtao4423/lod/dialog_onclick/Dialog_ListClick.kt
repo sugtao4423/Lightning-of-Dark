f49a25f5c669339f4bf9072b294d44feca56eab3
sugtao4423 <sugtao4423@gmail.com>
2019-08-05 18:08:25 +0900

    Dialog_ListClickのリファクタリング
    onItemClickが肥大化しすぎ

--- app/src/main/java/sugtao4423/lod/dialog_onclick/Dialog_ListClick.kt
+++ app/src/main/java/sugtao4423/lod/dialog_onclick/Dialog_ListClick.kt
@@ -21,122 +21,136 @@ import java.util.regex.Pattern
 class Dialog_ListClick(private val status: Status, private val listData: ArrayList<Status>, private val dialog: AlertDialog) :
         AdapterView.OnItemClickListener {
 
+    private lateinit var context: Context
+
     override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         if (parent == null) {
             return
         }
         dialog.dismiss()
-        val context = parent.context
+        context = parent.context
         val clickedText = parent.getItemAtPosition(position) as String
 
-        if (clickedText == context.getString(R.string.extract_with_regex)) {
-            val regView = View.inflate(context, R.layout.reg_dialog, null)
-            val regEdit = regView.findViewById<EditText>(R.id.regDialogEdit)
-            val gridLayout = regView.findViewById<GridLayout>(R.id.regDialogGrid)
-            val regItems = arrayOf(".", "*", "|", "+", "?", "\\", "^", "$", "(", ")", "[", "]", "{", "}")
-            val params = LinearLayout.LayoutParams(130, 130)
-            regItems.map {
-                val btn = Button(context).apply {
-                    layoutParams = params
-                    text = it
-                    setOnClickListener(Dialog_regButtonClick(regEdit))
-                }
-                gridLayout.addView(btn)
-            }
-
-            val pref = PreferenceManager.getDefaultSharedPreferences(context)
-            regEdit.setText(pref.getString(Keys.REGULAR_EXPRESSION, ""))
-            AlertDialog.Builder(context).apply {
-                setTitle(R.string.input_regex)
-                setView(regView)
-                setNegativeButton(R.string.cancel, null)
-                setPositiveButton(R.string.ok) { _, _ ->
-                    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                    imm.hideSoftInputFromWindow(regEdit.windowToken, 0)
-
-                    val editReg = regEdit.text.toString()
-                    pref.edit().putString(Keys.REGULAR_EXPRESSION, editReg).commit()
-                    val pattern = Pattern.compile(editReg, Pattern.DOTALL)
-                    val adapter = TweetListAdapter(context)
-                    var find = 0
-                    listData.map {
-                        if (pattern.matcher(it.text).find()) {
-                            adapter.add(it)
-                            find++
-                        }
-                    }
-                    if (find == 0) {
-                        ShowToast(context.applicationContext, R.string.nothing)
-                    } else {
-                        val l = TweetListView(context)
-                        l.adapter = adapter
-                        adapter.onItemClickListener = ListViewListener()
-                        adapter.onItemLongClickListener = ListViewListener()
-                        AlertDialog.Builder(context).setView(l).show()
-                        val resultCount = context.getString(R.string.param_regex_result_count, listData.size, find)
-                        ShowToast(context.applicationContext, resultCount, Toast.LENGTH_LONG)
-                    }
-                }
-                show()
-            }
-        } else if (clickedText.startsWith("http") || clickedText.startsWith("ftp")) {
-            val image = Regex.media_image.matcher(clickedText)
-            val video = Regex.media_video.matcher(clickedText)
-            val gif = Regex.media_gif.matcher(clickedText)
-            val state = Regex.statusUrl.matcher(clickedText)
-            val intent = when {
-                image.find() -> {
-                    val urls = arrayListOf<String>()
-                    status.mediaEntities.map {
-                        if (!Utils.isVideoOrGif(it)) {
-                            urls.add(it.mediaURLHttps)
-                        }
-                    }
-                    val pos = urls.indexOf(clickedText)
-                    Intent(context, ImageFragmentActivity::class.java).apply {
-                        putExtra(ImageFragmentActivity.INTENT_EXTRA_KEY_URLS, urls.toTypedArray())
-                        putExtra(ImageFragmentActivity.INTENT_EXTRA_KEY_POSITION, pos)
-                    }
-                }
-                video.find() -> {
-                    Intent(context, ShowVideo::class.java).apply {
-                        putExtra(ShowVideo.INTENT_EXTRA_KEY_URL, clickedText)
-                        putExtra(ShowVideo.INTENT_EXTRA_KEY_TYPE, ShowVideo.TYPE_VIDEO)
-                    }
-                }
-                gif.find() -> {
-                    Intent(context, ShowVideo::class.java).apply {
-                        putExtra(ShowVideo.INTENT_EXTRA_KEY_URL, clickedText)
-                        putExtra(ShowVideo.INTENT_EXTRA_KEY_TYPE, ShowVideo.TYPE_GIF)
-                    }
-                }
-                state.find() -> {
-                    Intent(context, IntentActivity::class.java).apply {
-                        putExtra(IntentActivity.TWEET_ID, state.group(Regex.statusUrlStatusIdGroup).toLong())
-                    }
-                }
-                else -> {
-                    ChromeIntent(context, Uri.parse(clickedText))
-                    return
-                }
-            }
-            context.startActivity(intent)
-        } else if (clickedText == context.getString(R.string.open_in_browser)) {
-            val orig = if (status.isRetweet) status.retweetedStatus else status
-            val tweetSn = orig.user.screenName
-            val tweetId = orig.id.toString()
-            val url = "https://twitter.com/$tweetSn/status/$tweetId"
-            context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
-        } else if (clickedText.startsWith("@")) {
-            val intent = Intent(context, UserPage::class.java)
-            val userSn = clickedText.substring(1)
-            if (status.user.screenName == userSn) {
-                intent.putExtra(UserPage.INTENT_EXTRA_KEY_USER_OBJECT, status.user)
-            } else {
-                intent.putExtra(UserPage.INTENT_EXTRA_KEY_USER_SCREEN_NAME, userSn)
-            }
-            context.startActivity(intent)
+        when {
+            clickedText == context.getString(R.string.extract_with_regex) -> extractTweetsWithRegex()
+            clickedText.contains(Regex("^(http|ftp)")) -> openUrl(clickedText)
+            clickedText == context.getString(R.string.open_in_browser) -> openInBrowser()
+            clickedText.startsWith("@") -> openUserPage(clickedText.substring(1))
         }
+    }
+
+    private fun extractTweetsWithRegex() {
+        val regView = View.inflate(context, R.layout.reg_dialog, null)
+        val regEdit = regView.findViewById<EditText>(R.id.regDialogEdit)
+        val gridLayout = regView.findViewById<GridLayout>(R.id.regDialogGrid)
+        val regItems = arrayOf(".", "*", "|", "+", "?", "\\", "^", "$", "(", ")", "[", "]", "{", "}")
+        val params = LinearLayout.LayoutParams(130, 130)
+        regItems.map {
+            val btn = Button(context).apply {
+                layoutParams = params
+                text = it
+                setOnClickListener(Dialog_regButtonClick(regEdit))
+            }
+            gridLayout.addView(btn)
+        }
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(context)
+        regEdit.setText(pref.getString(Keys.REGULAR_EXPRESSION, ""))
+        AlertDialog.Builder(context).also {
+            it.setTitle(R.string.input_regex)
+            it.setView(regView)
+            it.setNegativeButton(R.string.cancel, null)
+            it.setPositiveButton(R.string.ok) { _, _ ->
+                val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(regEdit.windowToken, 0)
+
+                val editReg = regEdit.text.toString()
+                pref.edit().putString(Keys.REGULAR_EXPRESSION, editReg).commit()
+                val pattern = Pattern.compile(editReg, Pattern.DOTALL)
+                val adapter = TweetListAdapter(context)
+                var find = 0
+                listData.map { status ->
+                    if (pattern.matcher(status.text).find()) {
+                        adapter.add(status)
+                        find++
+                    }
+                }
+                if (find == 0) {
+                    ShowToast(context.applicationContext, R.string.nothing)
+                } else {
+                    val l = TweetListView(context)
+                    l.adapter = adapter
+                    adapter.onItemClickListener = ListViewListener()
+                    adapter.onItemLongClickListener = ListViewListener()
+                    AlertDialog.Builder(context).setView(l).show()
+                    val resultCount = context.getString(R.string.param_regex_result_count, listData.size, find)
+                    ShowToast(context.applicationContext, resultCount, Toast.LENGTH_LONG)
+                }
+            }
+            it.show()
+        }
+    }
+
+    private fun openUrl(urlText: String) {
+        val image = Regex.media_image.matcher(urlText)
+        val video = Regex.media_video.matcher(urlText)
+        val gif = Regex.media_gif.matcher(urlText)
+        val state = Regex.statusUrl.matcher(urlText)
+        val intent = when {
+            image.find() -> {
+                val urls = arrayListOf<String>()
+                status.mediaEntities.map {
+                    if (!Utils.isVideoOrGif(it)) {
+                        urls.add(it.mediaURLHttps)
+                    }
+                }
+                val pos = urls.indexOf(urlText)
+                Intent(context, ImageFragmentActivity::class.java).apply {
+                    putExtra(ImageFragmentActivity.INTENT_EXTRA_KEY_URLS, urls.toTypedArray())
+                    putExtra(ImageFragmentActivity.INTENT_EXTRA_KEY_POSITION, pos)
+                }
+            }
+            video.find() -> {
+                Intent(context, ShowVideo::class.java).apply {
+                    putExtra(ShowVideo.INTENT_EXTRA_KEY_URL, urlText)
+                    putExtra(ShowVideo.INTENT_EXTRA_KEY_TYPE, ShowVideo.TYPE_VIDEO)
+                }
+            }
+            gif.find() -> {
+                Intent(context, ShowVideo::class.java).apply {
+                    putExtra(ShowVideo.INTENT_EXTRA_KEY_URL, urlText)
+                    putExtra(ShowVideo.INTENT_EXTRA_KEY_TYPE, ShowVideo.TYPE_GIF)
+                }
+            }
+            state.find() -> {
+                Intent(context, IntentActivity::class.java).apply {
+                    putExtra(IntentActivity.TWEET_ID, state.group(Regex.statusUrlStatusIdGroup).toLong())
+                }
+            }
+            else -> {
+                ChromeIntent(context, Uri.parse(urlText))
+                return
+            }
+        }
+        context.startActivity(intent)
+    }
+
+    private fun openInBrowser() {
+        val orig = if (status.isRetweet) status.retweetedStatus else status
+        val tweetSn = orig.user.screenName
+        val tweetId = orig.id.toString()
+        val url = "https://twitter.com/$tweetSn/status/$tweetId"
+        context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+    }
+
+    private fun openUserPage(userScreenName: String) {
+        val intent = Intent(context, UserPage::class.java)
+        if (status.user.screenName == userScreenName) {
+            intent.putExtra(UserPage.INTENT_EXTRA_KEY_USER_OBJECT, status.user)
+        } else {
+            intent.putExtra(UserPage.INTENT_EXTRA_KEY_USER_SCREEN_NAME, userScreenName)
+        }
+        context.startActivity(intent)
     }
 
 }
