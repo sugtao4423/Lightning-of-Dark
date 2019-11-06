25f2142a8e45a94df7ba04e35cc96cba30a53059
sugtao4423 <sugtao4423@gmail.com>
2019-11-06 14:22:27 +0900

    @Suppress("unused") musicNotificationListener

--- app/src/main/java/sugtao4423/lod/playing_music_data/MusicNotificationListener.kt
+++ app/src/main/java/sugtao4423/lod/playing_music_data/MusicNotificationListener.kt
@@ -3,6 +3,7 @@ package sugtao4423.lod.playing_music_data
 import android.service.notification.NotificationListenerService
 
 class MusicNotificationListener : NotificationListenerService() {
+    @Suppress("unused")
     fun musicNotificationListener() {
     }
 }
