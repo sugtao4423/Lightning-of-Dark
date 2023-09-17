569e9dec26130002e8911ffaca91fc49ccd918fa
sugtao4423 <sugtao4423@gmail.com>
2023-09-18 02:30:47 +0900

    Create GraphQL CreateTweet logic

--- app/src/main/java/sugtao4423/twitterweb4j/TwitterWeb4j.kt
+++ app/src/main/java/sugtao4423/twitterweb4j/TwitterWeb4j.kt
@@ -1,6 +1,9 @@
 package sugtao4423.twitterweb4j
 
+import sugtao4423.twitterweb4j.body.CreateTweetBody
+import sugtao4423.twitterweb4j.parser.JsonParserGraphQL
 import sugtao4423.twitterweb4j.parser.JsonParserV1
+import sugtao4423.twitterweb4j.url.UrlGraphQL
 import sugtao4423.twitterweb4j.url.UrlV1
 import twitter4j.JSONException
 import twitter4j.Paging
@@ -10,6 +13,7 @@ import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
+import sugtao4423.twitterweb4j.model.Status as StatusV2
 
 class TwitterWeb4j(private val csrfToken: String, private val cookie: String) {
 
@@ -34,6 +38,14 @@ class TwitterWeb4j(private val csrfToken: String, private val cookie: String) {
         return JsonParserV1.parseStatusesArray(response)
     }
 
+    @Throws(IOException::class, JSONException::class)
+    fun createTweet(tweetText: String): StatusV2 {
+        val url = UrlGraphQL.createTweet
+        val body = CreateTweetBody.get(tweetText)
+        val response = post(url, body)
+        return JsonParserGraphQL.parseCreateTweet(response)
+    }
+
     private fun setSession(conn: HttpURLConnection) {
         conn.setRequestProperty("Cookie", cookie)
         conn.setRequestProperty("X-Csrf-Token", csrfToken)
@@ -41,13 +53,31 @@ class TwitterWeb4j(private val csrfToken: String, private val cookie: String) {
 
     @Throws(IOException::class)
     private fun get(url: String): String {
+        return access("GET", url)
+    }
+
+    @Throws(IOException::class)
+    private fun post(url: String, body: String, contentType: String = "application/json"): String {
+        return access("POST", url, body, contentType)
+    }
+
+    @Throws(IOException::class)
+    private fun access(
+        method: String, url: String, body: String? = null, contentType: String? = null
+    ): String {
         val u = URL(url)
         val conn = u.openConnection() as HttpsURLConnection
 
         conn.apply {
-            requestMethod = "GET"
+            requestMethod = method
             Connection.setBaseHeaders(this)
             setSession(this)
+            if (method == "POST" && body != null) {
+                setRequestProperty("Content-Type", contentType)
+                setRequestProperty("Content-Length", body.length.toString())
+                doOutput = true
+                outputStream.use { it.write(body.toByteArray()) }
+            }
         }
 
         val data = conn.inputStream.bufferedReader().readText()
