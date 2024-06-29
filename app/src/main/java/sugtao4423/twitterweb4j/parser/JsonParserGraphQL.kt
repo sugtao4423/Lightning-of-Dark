2ac8599492d4f2f9ca47e84c3b029b791abe246c
sugtao4423 <sugtao4423@gmail.com>
2024-06-30 07:01:22 +0900

    Refactor to use `vararg`

--- app/src/main/java/sugtao4423/twitterweb4j/parser/JsonParserGraphQL.kt
+++ app/src/main/java/sugtao4423/twitterweb4j/parser/JsonParserGraphQL.kt
@@ -8,7 +8,7 @@ import twitter4j.TwitterException
 object JsonParserGraphQL {
 
     @Throws(JSONException::class)
-    private fun nestedJsonObject(response: String, nests: List<String>): JSONObject {
+    private fun nestedJsonObject(response: String, vararg nests: String): JSONObject {
         var json = JSONObject(response)
         nests.forEach { json = json.getJSONObject(it) }
         return json
@@ -18,7 +18,7 @@ object JsonParserGraphQL {
     fun parseCreateTweet(response: String): Status {
         try {
             val statusJson = nestedJsonObject(
-                response, listOf("data", "create_tweet", "tweet_results", "result")
+                response, "data", "create_tweet", "tweet_results", "result"
             )
             return Status(statusJson)
         } catch (e: JSONException) {
@@ -29,7 +29,7 @@ object JsonParserGraphQL {
     @Throws(TwitterException::class)
     fun parseDeleteTweet(response: String) {
         try {
-            nestedJsonObject(response, listOf("data", "delete_tweet", "tweet_results"))
+            nestedJsonObject(response, "data", "delete_tweet", "tweet_results")
         } catch (e: JSONException) {
             throw TwitterException(e)
         }
@@ -39,7 +39,7 @@ object JsonParserGraphQL {
     fun parseCreateRetweet(response: String) {
         try {
             nestedJsonObject(
-                response, listOf("data", "create_retweet", "retweet_results", "result")
+                response, "data", "create_retweet", "retweet_results", "result"
             )
         } catch (e: JSONException) {
             throw TwitterException(e)
@@ -50,7 +50,7 @@ object JsonParserGraphQL {
     fun parseDeleteRetweet(response: String) {
         try {
             nestedJsonObject(
-                response, listOf("data", "unretweet", "source_tweet_results", "result")
+                response, "data", "unretweet", "source_tweet_results", "result"
             )
         } catch (e: JSONException) {
             throw TwitterException(e)
@@ -60,7 +60,7 @@ object JsonParserGraphQL {
     @Throws(TwitterException::class)
     fun parseFavoriteTweet(response: String) {
         try {
-            val data = nestedJsonObject(response, listOf("data"))
+            val data = nestedJsonObject(response, "data")
             data.getString("favorite_tweet")
         } catch (e: JSONException) {
             throw TwitterException(e)
@@ -70,7 +70,7 @@ object JsonParserGraphQL {
     @Throws(TwitterException::class)
     fun parseUnfavoriteTweet(response: String) {
         try {
-            val data = nestedJsonObject(response, listOf("data"))
+            val data = nestedJsonObject(response, "data")
             data.getString("unfavorite_tweet")
         } catch (e: JSONException) {
             throw TwitterException(e)
