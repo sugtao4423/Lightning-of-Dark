1bc373fb4d5c0aab40c50a586c8a76cfb98ce67f
sugtao4423 <sugtao4423@gmail.com>
2024-06-30 10:32:11 +0900

    Fix crash at `URLEntity`

--- app/src/main/java/sugtao4423/twitterweb4j/impl/URLEntityJSONImpl.kt
+++ app/src/main/java/sugtao4423/twitterweb4j/impl/URLEntityJSONImpl.kt
@@ -1,6 +1,7 @@
 package sugtao4423.twitterweb4j.impl
 
 import sugtao4423.twitterweb4j.model.EntityIndex
+import sugtao4423.twitterweb4j.nullString
 import twitter4j.JSONObject
 import twitter4j.URLEntity
 
@@ -8,12 +9,12 @@ data class URLEntityJSONImpl(private val json: JSONObject) : URLEntity, EntityIn
     java.io.Serializable {
 
     private val url = json.getString("url")
-    private val expandedURL = json.getString("expanded_url")
-    private val displayURL = json.getString("display_url")
+    private val expandedURL = json.nullString("expanded_url")
+    private val displayURL = json.nullString("display_url")
 
     override fun getText(): String = url
     override fun getURL(): String = url
-    override fun getExpandedURL(): String = expandedURL
-    override fun getDisplayURL(): String = displayURL
+    override fun getExpandedURL(): String? = expandedURL
+    override fun getDisplayURL(): String? = displayURL
 
 }
