cae2268a4b33aec324959f0f3c79b97fd8065494
sugtao4423 <sugtao4423@gmail.com>
2026-04-02 01:59:51 +0900

    Refactor http headers to map

--- app/src/main/java/sugtao4423/twitterweb4j/Connection.kt
+++ app/src/main/java/sugtao4423/twitterweb4j/Connection.kt
@@ -7,30 +7,18 @@ object Connection {
     private const val twitterWebBearer =
         "AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
 
-    private const val acceptLanguage = "en-US,en;q=0.9"
+    private val defaultHeaders = mapOf(
+        "Authorization" to "Bearer $twitterWebBearer",
+        "Accept-Language" to "en-US,en;q=0.9",
+        "Cache-Control" to "no-cache",
+        "Referer" to "https://x.com/",
+        "User-Agent" to "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+        "X-Twitter-Active-User" to "yes",
+        "X-Twitter-Auth-Type" to "OAuth2Session",
+        "X-Twitter-Client-Language" to "en",
+    )
 
-    private const val cacheControl = "no-cache"
-
-    private const val referer = "https://x.com/"
-
-    private const val userAgent =
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
-
-    private const val twitterActiveUser = "yes"
-
-    private const val twitterAuthType = "OAuth2Session"
-
-    private const val twitterClientLanguage = "en"
-
-    fun setBaseHeaders(conn: HttpURLConnection) {
-        conn.setRequestProperty("Authorization", "Bearer $twitterWebBearer")
-        conn.setRequestProperty("Accept-Language", acceptLanguage)
-        conn.setRequestProperty("Cache-Control", cacheControl)
-        conn.setRequestProperty("Referer", referer)
-        conn.setRequestProperty("User-Agent", userAgent)
-        conn.setRequestProperty("X-Twitter-Active-User", twitterActiveUser)
-        conn.setRequestProperty("X-Twitter-Auth-Type", twitterAuthType)
-        conn.setRequestProperty("X-Twitter-Client-Language", twitterClientLanguage)
-    }
+    fun setBaseHeaders(conn: HttpURLConnection) =
+        defaultHeaders.forEach { (k, v) -> conn.setRequestProperty(k, v) }
 
 }
