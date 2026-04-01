5e008d3f3d20f34b4bb816ed721f8d40bca77d64
sugtao4423 <sugtao4423@gmail.com>
2026-04-02 01:52:49 +0900

    Add http headers

--- app/src/main/java/sugtao4423/twitterweb4j/Connection.kt
+++ app/src/main/java/sugtao4423/twitterweb4j/Connection.kt
@@ -7,6 +7,12 @@ object Connection {
     private const val twitterWebBearer =
         "AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
 
+    private const val acceptLanguage = "en-US,en;q=0.9"
+
+    private const val cacheControl = "no-cache"
+
+    private const val referer = "https://x.com/"
+
     private const val userAgent =
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
 
@@ -18,6 +24,9 @@ object Connection {
 
     fun setBaseHeaders(conn: HttpURLConnection) {
         conn.setRequestProperty("Authorization", "Bearer $twitterWebBearer")
+        conn.setRequestProperty("Accept-Language", acceptLanguage)
+        conn.setRequestProperty("Cache-Control", cacheControl)
+        conn.setRequestProperty("Referer", referer)
         conn.setRequestProperty("User-Agent", userAgent)
         conn.setRequestProperty("X-Twitter-Active-User", twitterActiveUser)
         conn.setRequestProperty("X-Twitter-Auth-Type", twitterAuthType)
