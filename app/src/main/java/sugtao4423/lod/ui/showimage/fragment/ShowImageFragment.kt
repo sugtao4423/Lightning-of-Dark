cb42dc3281a52b60790e42c98bc224be35932308
sugtao4423 <sugtao4423@gmail.com>
2026-04-03 02:29:04 +0900

    Migrate to viewBinding: `ShowImageFragment`

--- app/src/main/java/sugtao4423/lod/ui/showimage/fragment/ShowImageFragment.kt
+++ app/src/main/java/sugtao4423/lod/ui/showimage/fragment/ShowImageFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import sugtao4423.lod.databinding.FragmentShowImageBinding
+import sugtao4423.lod.ui.loadUrl
 
 class ShowImageFragment : Fragment() {
 
@@ -17,15 +18,14 @@ class ShowImageFragment : Fragment() {
     private val viewModel: ShowImageFragmentViewModel by viewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentShowImageBinding.inflate(inflater, container, false).also {
-            it.lifecycleOwner = this
-            it.viewModel = viewModel
-        }
+        val binding = FragmentShowImageBinding.inflate(inflater, container, false)
         viewModel.imageUrl = requireArguments().getString(BUNDLE_KEY_URL)!!
+        binding.imageView.loadUrl(viewModel.imageUrl, viewModel.requestListener)
+        viewModel.isShowProgressBar.observe(viewLifecycleOwner) {
+            binding.progressBar.visibility = if (it) View.VISIBLE else View.GONE
+        }
         return binding.root
     }
 
