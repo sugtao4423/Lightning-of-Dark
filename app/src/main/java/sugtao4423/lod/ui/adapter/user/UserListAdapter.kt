2776359524bbf3cab9ed68ec746086228677f8c7
sugtao4423 <sugtao4423@gmail.com>
2026-04-03 18:18:56 +0900

    Rename `UserViewDataConverter` -> `UserListConverter`

--- app/src/main/java/sugtao4423/lod/ui/adapter/user/UserListAdapter.kt
+++ app/src/main/java/sugtao4423/lod/ui/adapter/user/UserListAdapter.kt
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import sugtao4423.lod.App
 import sugtao4423.lod.databinding.ListItemUserBinding
-import sugtao4423.lod.ui.adapter.converter.UserViewDataConverter
+import sugtao4423.lod.ui.adapter.converter.UserListConverter
 import sugtao4423.lod.ui.loadUrl
 import twitter4j.PagableResponseList
 import twitter4j.User
@@ -54,20 +54,20 @@ class UserListAdapter(private val context: Context) :
 
             root.setOnClickListener { vm.onClickUser(it, user) }
 
-            userIcon.loadUrl(UserViewDataConverter.userIconUrl(user))
+            userIcon.loadUrl(UserListConverter.userIconUrl(user))
 
-            userName.text = UserViewDataConverter.userNameAndScreenName(user)
+            userName.text = UserListConverter.userNameAndScreenName(user)
             userName.setTextSize(TypedValue.COMPLEX_UNIT_SP, vm.nameTextSize)
 
-            val isShowProtected = UserViewDataConverter.isShowProtected(user)
+            val isShowProtected = UserListConverter.isShowProtected(user)
             protectIcon.visibility = if (isShowProtected) View.VISIBLE else View.GONE
             protectIcon.typeface = vm.fontAwesomeTypeface
             protectIcon.setTextSize(TypedValue.COMPLEX_UNIT_SP, vm.protectIconSize)
 
-            userDescription.text = UserViewDataConverter.userDescription(user)
+            userDescription.text = UserListConverter.userDescription(user)
             userDescription.setTextSize(TypedValue.COMPLEX_UNIT_SP, vm.textSize)
 
-            userCountDetail.text = UserViewDataConverter.userCountDetail(user)
+            userCountDetail.text = UserListConverter.userCountDetail(user)
             userCountDetail.setTextSize(TypedValue.COMPLEX_UNIT_SP, vm.dateTextSize)
         }
     }
