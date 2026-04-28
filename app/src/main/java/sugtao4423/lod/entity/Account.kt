a076f14013bfa93d6066435efbe809fed77ee2b2
sugtao4423 <sugtao4423@gmail.com>
2026-04-28 18:39:36 +0900

    Change database column names to snake_case

--- app/src/main/java/sugtao4423/lod/entity/Account.kt
+++ app/src/main/java/sugtao4423/lod/entity/Account.kt
@@ -6,13 +6,13 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "accounts")
 data class Account(
-    @PrimaryKey @ColumnInfo(name = "id") val id: Long,
-    @ColumnInfo(name = "screenName") val screenName: String,
-    @ColumnInfo(name = "profileImageUrl") val profileImageUrl: String,
-    @ColumnInfo(name = "cookie") val cookie: String,
-    @ColumnInfo(name = "listAsTL") val listAsTL: Long = -1L,
-    @ColumnInfo(name = "autoLoadTLInterval") val autoLoadTLInterval: Int = 0,
-    @ColumnInfo(name = "selectListIds") val selectListIds: List<Long> = listOf(),
-    @ColumnInfo(name = "selectListNames") val selectListNames: List<String> = listOf(),
-    @ColumnInfo(name = "appStartLoadLists") val startAppLoadLists: List<String> = listOf()
+    @PrimaryKey val id: Long,
+    @ColumnInfo(name = "screen_name") val screenName: String,
+    @ColumnInfo(name = "profile_image_url") val profileImageUrl: String,
+    val cookie: String,
+    @ColumnInfo(name = "list_as_tl") val listAsTL: Long = -1L,
+    @ColumnInfo(name = "auto_load_tl_interval") val autoLoadTLInterval: Int = 0,
+    @ColumnInfo(name = "select_list_ids") val selectListIds: List<Long> = listOf(),
+    @ColumnInfo(name = "select_list_names") val selectListNames: List<String> = listOf(),
+    @ColumnInfo(name = "start_app_load_lists") val startAppLoadLists: List<String> = listOf()
 )
